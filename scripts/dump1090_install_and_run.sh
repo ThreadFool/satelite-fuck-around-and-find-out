b921a53174ef4689ef3633b3c5f8737046f4042d
Thread Fool <101286930+ThreadFool@users.noreply.github.com>
2025-12-14 23:30:31 +0100

    Add installation commands for dump1090

---
+++ scripts/dump1090_install_and_run.sh
@@ -0,0 +1,5 @@
+wget https://www.flightaware.com/adsb/piaware/files/packages/pool/piaware/f/flightaware-apt-repository/flightaware-apt-repository_1.2_all.deb
+sudo dpkg -i flightaware-apt-repository_1.2_all.deb
+sudo apt update
+sudo systemctl stop dump1090-fa
+dump1090-fa --interactive
